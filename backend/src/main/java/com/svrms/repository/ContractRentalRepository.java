5b4d9107eb1883d3e37e32fff997ff0ff9387efe
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2026-01-26 00:07:42 +0100

    Create ContractRentalRepository.java

---
+++ backend/src/main/java/com/svrms/repository/ContractRentalRepository.java
@@ -0,0 +1,6 @@
+package com.svrms.repository;
+
+import com.svrms.entity.ContractRental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRentalRepository extends JpaRepository<ContractRental, Long> {}
