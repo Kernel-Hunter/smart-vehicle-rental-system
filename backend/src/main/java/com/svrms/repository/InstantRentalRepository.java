c1b1697dfe4f1172b1f994a9fa822e4f17befdbe
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2026-01-27 00:09:30 +0100

    Delete backend/src/main/java/com/svrms/repository/InstantRentalRepository.java

--- backend/src/main/java/com/svrms/repository/InstantRentalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.svrms.repository;
-
-import com.svrms.entity.InstantRental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstantRentalRepository extends JpaRepository<InstantRental, Long> {}
