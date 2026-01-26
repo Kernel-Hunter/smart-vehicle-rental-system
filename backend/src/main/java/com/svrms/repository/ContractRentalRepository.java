2ca09d563f2858adf818afa619b4224a2a34473a
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2026-01-27 00:09:18 +0100

    Delete backend/src/main/java/com/svrms/repository/ContractRentalRepository.java

--- backend/src/main/java/com/svrms/repository/ContractRentalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.svrms.repository;
-
-import com.svrms.entity.ContractRental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractRentalRepository extends JpaRepository<ContractRental, Long> {}
