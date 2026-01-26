72c6b538dcfc67d9d65b63aec36e98ec58d792cf
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2026-01-27 00:14:18 +0100

    Update RentalRepository.java

--- backend/src/main/java/com/svrms/repository/RentalRepository.java
+++ backend/src/main/java/com/svrms/repository/RentalRepository.java
@@ -1,1 +1,11 @@
+package com.svrms.repository;
 
+import com.svrms.entity.Rental;
+import com.svrms.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findByVehicleAndStatus(Vehicle vehicle, Rental.RentalStatus status);
+}
