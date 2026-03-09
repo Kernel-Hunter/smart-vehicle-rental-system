ac98bee5f0e67b9e75c0cbf76fca80ddb103796b
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2026-03-09 01:53:54 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    // Required for Vuetify to work correctly in production builds
     'process.env': {}
   }
 })
